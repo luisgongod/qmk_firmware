6e9681d44ebc846c88ebb03bd9b332b4cb258eea
Luis Gonzalez <LGodinez@oceaneering.com>
2022-09-12 15:34:09 +0200

    combo enable

--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -19,7 +19,7 @@ AUDIO_ENABLE = no           # Audio output
 RGB_MATRIX_ENABLE = no
 RGB_MATRIX_DRIVER = WS2812
 LTO_ENABLE = yes
-
+COMBO_ENABLE=yes
 # if firmware size over limit, try this option
 # LTO_ENABLE = yes
 
