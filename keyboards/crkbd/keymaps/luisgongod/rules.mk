36ff057b49fa446169785dfbd79098a9178b8021
Luis Gonzalez <LGodinez@oceaneering.com>
2022-09-13 20:59:26 +0200

    ccccc combo

---
+++ keyboards/crkbd/keymaps/luisgongod/rules.mk
@@ -0,0 +1,8 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = no
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+
+VPATH  +=  keyboards/gboards/
+COMBO_ENABLE=yes
