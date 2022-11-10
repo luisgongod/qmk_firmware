972ea3b7f1ceab837e47393d29c17069c8c3c787
Luis Gonzalez <LGodinez@oceaneering.com>
2022-11-10 19:06:58 +0100

    reviu 41 back

--- keyboards/reviung/reviung41/keymaps/luisgongod copy/rules.mk
+++
@@ -1,8 +0,0 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
-OLED_ENABLE     = no
-OLED_DRIVER     = SSD1306
-LTO_ENABLE      = yes
-
-VPATH  +=  keyboards/gboards/
-COMBO_ENABLE=yes
