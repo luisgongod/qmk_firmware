285d1f47f60ebe206a878138e3e7c81f0030334e
Luis Gonzalez <LGodinez@oceaneering.com>
2023-02-03 11:10:07 +0100

    cirque on corne

--- keyboards/crkbd/keymaps/luisgongod/config.h
+++ keyboards/crkbd/keymaps/luisgongod/config.h
@@ -1,3 +1,5 @@
+// Corne
+
 /*
 This is the c configuration file for the keymap
 
@@ -24,8 +26,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
-// #define MASTER_RIGHT
+// #define MASTER_LEFT
+#define MASTER_RIGHT
 // #define EE_HANDS
 
 //#define TAPPING_FORCE_HOLD
@@ -52,3 +54,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define COMBO_VARIABLE_LEN
 #define COMBO_TERM 50
+
+#define CIRQUE_PINNACLE_TAP_ENABLE
+#define CIRQUE_PINNACLE_SCROLL_ENABLE
+#define CIRQUE_PINNACLE_SECONDARY_TAP_ENABLE
+#define POINTING_DEVICE_GESTURES_SCROLL_ENABLE 
+#define POINTING_DEVICE_ROTATION_90	
+#define POINTING_DEVICE_INVERT_X
+#define POINTING_DEVICE_INVERT_Y
--- keyboards/crkbd/keymaps/luisgongod/rules.mk
+++ keyboards/crkbd/keymaps/luisgongod/rules.mk
@@ -1,3 +1,4 @@
+#Corne
 MOUSEKEY_ENABLE = yes    # Mouse keys
 RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
 OLED_ENABLE     = no
@@ -7,3 +8,6 @@ LTO_ENABLE      = yes
 VPATH  +=  keyboards/gboards/
 COMBO_ENABLE=yes
 SEND_STRING_ENABLE = yes
+
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
