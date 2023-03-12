50aa3bea39628cc7b108da9fd1b54953dd651385
luisgongod <luis.gongod@gmail.com>
2023-03-12 19:58:09 +0100

    sofle luisgongod added

--- keyboards/sofle/keymaps/luisgongod/config.h
+++ keyboards/sofle/keymaps/luisgongod/config.h
@@ -4,3 +4,15 @@
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
+
+
+// #define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+//#define TAPPING_FORCE_HOLD
+//#define TAPPING_TERM 100
+#define TAPPING_TOGGLE 2
+
+#define COMBO_VARIABLE_LEN
+#define COMBO_TERM 50
