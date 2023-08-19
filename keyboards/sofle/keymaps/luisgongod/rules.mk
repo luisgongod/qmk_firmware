fe808e732df93d14e7a3830f890a6cf2cf27b5ee
luisgongod <luis.gongod@gmail.com>
2023-08-19 22:59:12 +0200

    changes

--- keyboards/sofle/keymaps/luisgongod/rules.mk
+++ keyboards/sofle/keymaps/luisgongod/rules.mk
@@ -1,4 +1,4 @@
-
+MOUSEKEY_ENABLE = yes    # Mouse keys
 OLED_ENABLE = no
 OLED_DRIVER = SSD1306
 ENCODER_ENABLE = yes
@@ -8,3 +8,6 @@ EXTRAKEY_ENABLE = yes
 VPATH  +=  keyboards/gboards/
 COMBO_ENABLE=yes
 SEND_STRING_ENABLE = yes
+
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
